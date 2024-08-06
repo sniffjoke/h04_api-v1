229d8344aaad53fe465fc09095e6a36f25a96977
sniffjoke <sniffjoke@gmail.com>
2024-08-06 14:56:41 +0200

    add new query for blogs

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -9,6 +9,5 @@ export const queryHelper = (query: { [key: string]: string | undefined }) => {
         page: query.page ? Number(query.page) : 1,
         pageSize: query.pageSize !== undefined ? +query.pageSize : 10,
         totalCount: query.totalCount ? +query.totalCount : 1,
-        searchNameTerm: query.searchNameTerm ? query.searchNameTerm : null,
     }
 }
