9d5668a3c15c65190ce705d856f198d7c064ef40
sniffjoke <sniffjoke@gmail.com>
2024-08-06 14:49:54 +0200

    add new query for blogs

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,9 +1,14 @@
 export const queryHelper = (query: { [key: string]: string | undefined }) => {
     return {
-        pageNumber: query.pageNumber ? +query.pageNumber : 1,
+        // pageNumber: query.pageNumber ? +query.pageNumber : 1,
+        // pageSize: query.pageSize !== undefined ? +query.pageSize : 10,
+        // sortBy: query.sortBy ? +query.sortBy : 'createdAt',
+        // sortDirection: query.sortDirection === 'asc' ? +query.sortDirection : 'desc',
+        // searchNameTerm: query.searchNameTerm ? query.searchNameTerm : null,
+        pagesCount: query.pagesCount ? +query.pagesCount : 1,
+        page: query.page ? Number(query.page) : 1,
         pageSize: query.pageSize !== undefined ? +query.pageSize : 10,
-        sortBy: query.sortBy ? +query.sortBy : 'createdAt',
-        sortDirection: query.sortDirection === 'asc' ? +query.sortDirection : 'desc',
+        totalCount: query.totalCount ? +query.totalCount : 1,
         searchNameTerm: query.searchNameTerm ? query.searchNameTerm : null,
     }
 }
