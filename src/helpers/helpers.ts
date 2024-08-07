7c581ea95428b69eeef5d670f3c700ddc114650c
sniffjoke <sniffjoke@gmail.com>
2024-08-07 02:19:39 +0200

    edit helper

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -3,9 +3,9 @@ import {db} from "../db/mongo-db";
 export const queryHelper = async (query: { [key: string]: string | undefined }, searchProperty: 'blogs' | 'posts', blogId?: string) => {
     const searchNameTerm = query.searchNameTerm ? query.searchNameTerm : null
     const queryName = searchNameTerm !== null ? searchNameTerm : ''
-    // const filter = searchProperty === 'blogs' ? {name: {$regex: queryName, $options: "i"},} : {};
-    const filter = blogId ? blogId : {}
-    const totalCount = await db.collection(searchProperty).countDocuments({blogId: filter})
+    const filterName = searchProperty === 'blogs' ? {name: {$regex: queryName, $options: "i"},} : {};
+    const filterId = blogId ? blogId : {}
+    const totalCount = await db.collection(searchProperty).countDocuments(blogId ? {blogId: filterId} : filterName)
     const pageSize = query.pageSize !== undefined ? +query.pageSize : 10
     const pagesCount = Math.ceil(totalCount / +pageSize)
 
